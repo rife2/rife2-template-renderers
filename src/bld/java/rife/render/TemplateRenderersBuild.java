313121cfcc32e30fa268ed1931ad89db057f8380
Erik C. Thauvin <erik@thauvin.net>
2024-07-31 17:17:02 -0700

    Version 1.1.6

--- src/bld/java/rife/render/TemplateRenderersBuild.java
+++ src/bld/java/rife/render/TemplateRenderersBuild.java
@@ -40,7 +40,7 @@ public class TemplateRenderersBuild extends Project {
     public TemplateRenderersBuild() {
         pkg = "rife.render";
         name = "rife2-template-renderers";
-        version = version(1, 1, 6, "SNAPSHOT");
+        version = version(1, 1, 6);
 
         javaRelease = 17;
         downloadSources = true;
