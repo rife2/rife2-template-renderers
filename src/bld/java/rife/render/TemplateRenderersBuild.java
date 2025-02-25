10ea21245b047d461eec846c505dd31a6de9f4d6
Erik C. Thauvin <erik@thauvin.net>
2025-02-25 09:35:27 -0800

    Bump JUnit to version 5.12.0

--- src/bld/java/rife/render/TemplateRenderersBuild.java
+++ src/bld/java/rife/render/TemplateRenderersBuild.java
@@ -50,9 +50,9 @@ public class TemplateRenderersBuild extends Project {
         scope(compile)
                 .include(dependency("com.uwyn.rife2", "rife2", version(1, 9, 1)));
         scope(test)
-                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 11, 4)))
-                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 11, 4)))
                 .include(dependency("org.assertj", "assertj-core", version(3, 27, 2)));
+                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 12, 0)))
+                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 12, 0)))
 
         javadocOperation().javadocOptions()
                 .docTitle("<a href=\"https://rife2.com\">RIFE2</a> Template Renderers")
