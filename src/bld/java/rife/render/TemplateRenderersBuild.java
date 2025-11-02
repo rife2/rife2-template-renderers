fa92e5b39a24e8ffdcd8b60b79ef077803e834da
Erik C. Thauvin <erik@thauvin.net>
2025-11-01 20:29:47 -0700

    Bump Testing Helpers to version 0.9.4

--- src/bld/java/rife/render/TemplateRenderersBuild.java
+++ src/bld/java/rife/render/TemplateRenderersBuild.java
@@ -57,11 +57,7 @@ public class TemplateRenderersBuild extends Project {
                         version(4, 9, 8)));
         scope(test)
                 .include(dependency("com.uwyn.rife2", "bld-extensions-testing-helpers",
-                        version(0, 9, 0, "SNAPSHOT")))
-                .include(dependency("org.junit.jupiter", "junit-jupiter",
-                        version(5, 13, 4)))
-                .include(dependency("org.junit.platform", "junit-platform-console-standalone",
-                        version(1, 13, 4)))
+                        version(0, 9, 4)))
                 .include(dependency("com.squareup.okhttp3", "mockwebserver",
                 .include(dependency("org.junit.jupiter", "junit-jupiter", junit))
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone", junit))
