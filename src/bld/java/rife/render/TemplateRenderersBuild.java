85456925a7cbe09075e91b6181f7d51ef29a181c
Erik C. Thauvin <erik@thauvin.net>
2025-11-24 22:31:52 -0800

    Bump MockWebserver to version 5.3.2

--- src/bld/java/rife/render/TemplateRenderersBuild.java
+++ src/bld/java/rife/render/TemplateRenderersBuild.java
@@ -57,7 +57,7 @@ public class TemplateRenderersBuild extends Project {
                 .include(dependency("com.uwyn.rife2", "bld-extensions-testing-helpers",
                         version(0, 9, 4)))
                 .include(dependency("com.squareup.okhttp3", "mockwebserver",
-                        version(5, 3, 0)))
+                        version(5, 3, 2)))
                 .include(dependency("org.junit.jupiter", "junit-jupiter", junit))
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone", junit))
                 .include(dependency("org.assertj", "assertj-core",
